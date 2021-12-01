08c6d5b6a5396aa490fa8261b68e9e3dd45f013c
Sebastian Widua <seppel3210@gmail.com>
2021-12-01 08:59:16 +0100

    Solve day 1 part 2

---
+++ src/bin/1_2.rs
@@ -0,0 +1,18 @@
+#![feature(array_windows)]
+
+fn main() {
+    let input = advent_of_code_2021::input("1");
+    let depths: Vec<u32> = input
+        .lines()
+        .map(|l| l.trim().parse().unwrap())
+        .collect();
+    let depth_sums: Vec<u32> = depths
+        .windows(3)
+        .map(|window| window.iter().sum())
+        .collect();
+    let count: u32 = depth_sums
+        .array_windows()
+        .map(|[left, right]| if right > left { 1 } else { 0 })
+        .sum();
+    println!("{count}");
+}
