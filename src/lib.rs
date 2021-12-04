44738ac2c8764329d97db745cde7cc1ed831449e
Sebastian Widua <seppel3210@gmail.com>
2021-12-04 15:03:13 +0100

    Automatically fetch input from the website using reqwest

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
-use std::path::Path;
+#![feature(try_blocks)]
+use reqwest::{blocking::Client, cookie::Jar};
+use std::{error::Error, sync::Arc};
 
-pub fn input(path: impl AsRef<Path>) -> String {
-    let dir: &Path = "input".as_ref();
-    std::fs::read_to_string(dir.join(path)).unwrap()
+const URL: &'static str = "https://adventofcode.com";
+const TOKEN: &'static str = include_str!("../session_token.txt");
+
+pub fn input(path: &str) -> String {
+    let res: Result<_, Box<dyn Error>> = try {
+        let jar = Jar::default();
+        let url = URL.parse()?;
+        jar.add_cookie_str(&format!("session={TOKEN}"), &url);
+        let client = Client::builder().cookie_provider(Arc::new(jar)).build()?;
+
+        client
+            .get(url.join(&format!("2021/day/{path}/input"))?)
+            .send()?
+            .text()?
+    };
+    res.unwrap()
 }
