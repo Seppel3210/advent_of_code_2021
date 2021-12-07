8807bc5bb37a1d427b0d43706bf851cabb88db47
Sebastian Widua <seppel3210@gmail.com>
2021-12-08 00:05:38 +0100

    Solve day 7

--- src/bin/6.rs
+++ src/bin/6.rs
@@ -1,14 +1,14 @@
 fn main() {
     let input = advent_of_code_2021::input("6");
     let days = 256;
-    // the count of fish that were born at day i 
+    // the count of fish that were born at day i
     let mut fish_amount = vec![0; days + 9];
     for days_until_birth in input.trim().split(',').map(|s| s.parse::<usize>().unwrap()) {
         fish_amount[days_until_birth] += 1;
     }
     for i in 0..fish_amount.len() {
         let amount = fish_amount[i];
-        for day_born in (i+9..fish_amount.len()).step_by(7) {
+        for day_born in (i + 9..fish_amount.len()).step_by(7) {
             fish_amount[day_born] += amount;
         }
     }
---
+++ src/bin/7.rs
@@ -0,0 +1,17 @@
+#![feature(int_abs_diff)]
+
+use std::collections::BTreeMap;
+
+fn main() {
+    let input = advent_of_code_2021::input("7");
+
+    let mut v: Vec<u32> = input
+        .trim()
+        .split(',')
+        .map(|x| x.parse().unwrap())
+        .collect();
+    let len = v.len();
+    let (_, &mut median, _) = v.select_nth_unstable(len / 2);
+    let cost: u32 = v.iter().map(|x| x.abs_diff(median)).sum();
+    println!("{cost}");
+}
